a0eaf9efc198e9ef74b6a537ac462ca558158dde
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2025-02-15 22:53:45 -0300

    .

--- Ejemplos_ADO/Ej09_CrearPersona/Program.cs
+++ Ejemplos_ADO/Ej09_CrearPersona/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ej07_Consulta_Personas.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+void CrearNuevaPersona(Persona nueva)
+{
+    var cadenaConexion = "workstation id=GuidoAlumnoDB.mssql.somee.com;packet size=4096;user id=guidoagustin_SQLLogin_1;pwd=fmvfrm1h5h;data source=GuidoAlumnoDB.mssql.somee.com;persist security info=False;initial catalog=GuidoAlumnoDB;TrustServerCertificate=True";
+
+    var query =
+    @"insert into Personas (Nombre) 
+      OUTPUT INSERTED.ID 
+      values (@Nombre)";
+
+    using var conexion = new SqlConnection(cadenaConexion);   //hace que se cierre
+    conexion.Open();
+
+    var comando = new SqlCommand(query, conexion);
+    comando.Parameters.AddWithValue("@Nombre", nueva.Nombre);
+
+    var newId = comando.ExecuteScalar();
+
+    Console.WriteLine(newId);
+}
+
+
+//ejemplo de uso
+
+var nueva = new Persona { 
+    Nombre="Marianela",
+};
+
+CrearNuevaPersona(nueva);
+
+Console.WriteLine(nueva);
--- Ejemplos_ADO/Readme.md
+++ Ejemplos_ADO/Readme.md
@@ -1,12 +1,12 @@
 
 
-## Libreria
+### Libreria
 ```
 Microsoft.Data.SqlClient
 ```
 
 
-## Ejemplos de cadenas de conexión
+### Ejemplos de cadenas de conexión
 ```csharp
 var cadenaconexion = "workstation id=MaximaAlumnosBD.mssql.somee.com;packet size=4096;user id=Maxima1428_SQLLogin_1;pwd=si8gmykxbl;data source=MaximaAlumnosBD.mssql.somee.com;persist security info=False;initial catalog=MaximaAlumnosBD;TrustServerCertificate=True";
 ```
